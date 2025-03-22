be91a78a86a638143d8eba306ac1fe4d54aac058
marciel124 <marciel8anthony@gmail.com>
2025-03-22 21:28:59 +0000

    carta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,9 +2,14 @@
 
 int main(){
  char estado_1[3];
- 
+ char numero_da_carta_1[3];
+
     printf("digite o estado:");
     scanf("%s",estado_1);
-    printf("o estado e;%s",estado_1);
+    printf("o estado e:%s",estado_1);
+
+    printf("digite o numero da carta:");
+    scanf("%s", numero_da_carta_1);
+    printf("o numero da carta e:%s",numero_da_carta_1);
   return 0;  
 }
