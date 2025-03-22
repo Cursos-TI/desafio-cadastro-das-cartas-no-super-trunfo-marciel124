5bc214c025363e46d9148a84d98df6fe66eeb46a
marciel124 <marciel8anthony@gmail.com>
2025-03-22 16:09:09 +0000

    int

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,19 +1,11 @@
 #include <stdio.h>
 
+int main(){
 char Estado_1[3];
 char Número_da_carta_1[3];
 char Cidade_1[10];
 int População_1;
-float Área_1;
+float Área_1; 
 
-int main() {
-  printf("digite os dados da carta1/n");
-  
- 
-  printf("digite o estado: /n");
-  scanf("%s", estado_1);
-
-  printf("digite o numero da carta/n")
-  scanf("%s", numero da Número_da_carta_1);
   return 0;  
 }
