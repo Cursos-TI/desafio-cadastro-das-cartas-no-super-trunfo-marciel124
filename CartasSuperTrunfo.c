9a26198297fe5543942ec22e4fda012bb8ea19b8
marciel124 <marciel8anthony@gmail.com>
2025-03-24 21:33:40 +0000

    populaçao

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -3,6 +3,8 @@
 int main(){
  char estado_1[3];
  char numero_da_carta_1[3];
+ char nome_da_cidade_1[3];
+ int  populaçao_1;
 
     printf("digite o estado:");
     scanf("%s",estado_1);
@@ -11,5 +13,14 @@ int main(){
     printf("digite o numero da carta:");
     scanf("%s", numero_da_carta_1);
     printf("o numero da carta e:%s",numero_da_carta_1);
-  return 0;  
+
+    printf("digite o nome da cidade:");
+    scanf("%s",nome_da_cidade_1);
+    printf("o nome da cidade e:%s",nome_da_cidade_1); 
+   
+   printf("digite a populaçao;");
+   scanf("%d",populaçao_1);
+   printf("digite a populaçao");
+   
+    return 0;
 }
