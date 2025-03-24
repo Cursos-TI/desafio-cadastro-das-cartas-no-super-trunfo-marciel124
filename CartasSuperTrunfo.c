49746fd3488fda15d402aeb1eb00fd621e3ebd6f
marciel124 <marciel8anthony@gmail.com>
2025-03-24 22:24:40 +0000

    teste

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,23 +4,23 @@ int main(){
  char estado_1[3];
  char numero_da_carta_1[3];
  char nome_da_cidade_1[3];
- int  populaçao_1;
+ int  populacao_1;
 
     printf("digite o estado:");
     scanf("%s",estado_1);
-    printf("o estado e:%s",estado_1);
+    printf("o estado e: %s",estado_1);
 
     printf("digite o numero da carta:");
     scanf("%s", numero_da_carta_1);
-    printf("o numero da carta e:%s",numero_da_carta_1);
+    printf("o numero da carta e: %s",numero_da_carta_1);
 
-    printf("digite o nome da cidade:");
-    scanf("%s",nome_da_cidade_1);
-    printf("o nome da cidade e:%s",nome_da_cidade_1); 
+    printf("digite o nome da cidade: ");
+    scanf("%s", nome_da_cidade_1);
+    printf("o nome da cidade e: %s",nome_da_cidade_1); 
    
-   printf("digite a populaçao;");
-   scanf("%d",populaçao_1);
-   printf("digite a populaçao");
+   printf("digite a populaçao: ");
+   scanf("%d", &populacao_1);
+    
    
     return 0;
 }
